14d5a5f56943d6236fe57fd35f4d21b39057116b
Juanjo <juanjotorres90@gmail.com>
2019-11-01 17:25:25 +0100

    Cards & Menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $(function () {
 
 
-  $("h1").click(function () {
-    $(this).css("color", "red");
+  $(".filters-button").click(function () {
+    $(this).css({
+      "color": "#fff",
+      "background-color": "#0d8ac7"
+    });
+    $(".menu__lat-menu--closed").toggle("display");
   });
-
+  $("span").click(function () {
+    $(".menu__lat-menu--closed").hide();
+  });
 
 });
