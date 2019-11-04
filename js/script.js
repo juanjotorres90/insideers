a19720cc79307f14d26e1866e3d3571ee26a965b
Juanjo <juanjotorres90@gmail.com>
2019-11-04 18:11:35 +0100

    Search bar

--- js/script.js
+++ js/script.js
@@ -2,58 +2,93 @@ $(function () {
 
 
   $(".filters-button").click(function () {
-    $(this).css({
-      "color": "#fff",
-      "background-color": "#0d8ac7"
-    });
-    $(".menu__lat-menu--closed").toggle("display");
+    $(this).toggleClass("header__button--checked");
+    $(".menu__lat-menu--closed").slideToggle(800, 'swing');
   });
   $("span").click(function () {
     $(".menu__lat-menu--closed").hide();
+    $(".filters-button").removeClass("header__button--checked")
   });
 
+  $(".card__button").click(function () {
+    $(this).toggleClass("header__button--checked");
+  });
 
+  var buttonFilters = {};
+  var buttonFilter;
+
+  var qsRegex;
 
 
   var $grid = $('.container').isotope({
-    itemSelector: '.card'
+    itemSelector: '.card',
+    layoutMode: 'fitRows',
+    filter: function () {
+      var $this = $(this);
+      var searchResult = qsRegex ? $this.text().match(qsRegex) : true;
+      var buttonResult = buttonFilter ? $this.is(buttonFilter) : true;
+      return searchResult && buttonResult;
+    }
   });
 
 
+  $('.filters').on('click', '.button', function () {
+    var $this = $(this);
 
-var filters = {};
+    var $buttonGroup = $this.parents('.button-group');
+    var filterGroup = $buttonGroup.attr('data-filter-group');
 
-$('.filters').on( 'click', '.button', function( event ) {
-  var $button = $( event.currentTarget );
-  // get group key
-  var $buttonGroup = $button.parents('.button-group');
-  var filterGroup = $buttonGroup.attr('data-filter-group');
-  // set filter for group
-  filters[ filterGroup ] = $button.attr('data-filter');
-  // combine filters
-  var filterValue = concatValues( filters );
-  // set filter for Isotope
-  $grid.isotope({ filter: filterValue });
-});
+    buttonFilters[filterGroup] = $this.attr('data-filter');
 
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', '.button', function( event ) {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    var $button = $( event.currentTarget );
-    $button.addClass('is-checked');
+    buttonFilter = concatValues(buttonFilters);
+
+    $grid.isotope();
   });
-});
-  
-// flatten object by concatting values
-function concatValues( obj ) {
-  var value = '';
-  for ( var prop in obj ) {
-    value += obj[ prop ];
+
+  $('.button-group').each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', '.button', function (event) {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      var $button = $(event.currentTarget);
+      $button.addClass('is-checked');
+    });
+  });
+
+
+  function concatValues(obj) {
+    var value = '';
+    for (var prop in obj) {
+      value += obj[prop];
+    }
+    return value;
   }
-  return value;
-}
+
+
+
+  var $quicksearch = $('.quicksearch').keyup(debounce(function () {
+    qsRegex = new RegExp($quicksearch.val(), 'gi');
+    $grid.isotope();
+  }, 200));
+  var $quicksearchMobile = $('.quicksearch-mobile').keyup(debounce(function () {
+    qsRegex = new RegExp($quicksearchMobile.val(), 'gi');
+    $grid.isotope();
+  }, 200));
+
+
+  function debounce(fn, threshold) {
+    var timeout;
+    threshold = threshold || 100;
+    return function debounced() {
+      clearTimeout(timeout);
+      var args = arguments;
+      var _this = this;
+
+      function delayed() {
+        fn.apply(_this, args);
+      }
+      timeout = setTimeout(delayed, threshold);
+    };
+  }
 
 
 });
